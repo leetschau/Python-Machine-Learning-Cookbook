baac94ce411bc9a26074e5eba7b1a54b29a98e65
Li Chao <leetschau@gmail.com>
2017-12-09 21:53:47 +0800

    add some notebooks about time series data manipulation

--- Chapter08/convert_to_timeseries.py
+++ Chapter08/convert_to_timeseries.py
@@ -11,8 +11,8 @@ def convert_data_to_timeseries(input_file, column, verbose=False):
     end_date = str(int(data[-1,0] + 1)) + '-' + str(int(data[-1,1] % 12 + 1))
 
     if verbose:
-        print "\nStart date =", start_date
-        print "End date =", end_date
+        print("\nStart date =", start_date)
+        print("End date =", end_date)
 
     # Create a date sequence with monthly intervals
     dates = pd.date_range(start_date, end_date, freq='M')
@@ -21,7 +21,7 @@ def convert_data_to_timeseries(input_file, column, verbose=False):
     data_timeseries = pd.Series(data[:,column], index=dates)
 
     if verbose:
-        print "\nTime series data:\n", data_timeseries[:10]
+        print("\nTime series data:\n", data_timeseries[:10])
 
     return data_timeseries
 
